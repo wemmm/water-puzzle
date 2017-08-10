f46aa7aface9583ce49d7124fdc620157207e5f2
wemmm <jenny.wem@gmail.com>
2017-08-10 16:33:30 +0100

    Adds public folder, adds canvas to index.html

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const pug = require('pug')
 const app = express()
 
 app.set('view engine', 'pug');
+app.use(express.static('./public'));
+app.use(express.static('../public'));
 
 app.get('/', function (req, res) {
   res.render('index.pug');
