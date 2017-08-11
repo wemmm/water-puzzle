bf8be640f6c02614370603ad4bc7e25a71c861fa
wemmm <jenny.wem@gmail.com>
2017-08-11 15:57:45 +0100

    Adds function to transfer water between jug instances
    - Refactors waterPuzzleSpec.js to use dependent objects rather than fresh instances

--- public/javascript/waterPuzzle.js
+++ public/javascript/waterPuzzle.js
@@ -4,10 +4,34 @@ function waterPuzzle() {
   this.scales = new scales;
 }
 
-waterPuzzle.prototype.fillFromTap = function (jug) {
+waterPuzzle.prototype.fillJug = function (jug) {
   jug.current_capacity = jug.max_capacity
 };
 
-waterPuzzle.prototype.empty = function (jug) {
-  jug.current_capacity = 0
+waterPuzzle.prototype.fillFiveLitreJug = function () {
+  this.fiveLitreJug.current_capacity = this.fiveLitreJug.max_capacity
+};
+
+waterPuzzle.prototype.fillThreeLitreJug = function () {
+  this.threeLitreJug.current_capacity = this.threeLitreJug.max_capacity
+};
+
+waterPuzzle.prototype.emptyFiveLitreJug = function () {
+  this.fiveLitreJug.current_capacity = 0
+};
+
+waterPuzzle.prototype.emptyThreeLitreJug = function () {
+  this.threeLitreJug.current_capacity = 0
+};
+
+waterPuzzle.prototype.pourInto = function (fromJug, toJug) {
+  var availableSpace = (toJug.max_capacity - toJug.current_capacity)
+  var availableWater = (fromJug.current_capacity)
+  if (availableWater > availableSpace) {
+    toJug.current_capacity = toJug.max_capacity;
+    fromJug.current_capacity = (fromJug.current_capacity - availableSpace)
+  } else {
+    toJug.current_capacity += availableWater;
+    fromJug.current_capacity = 0
+  }
 };
--- spec/waterPuzzleSpec.js
+++ spec/waterPuzzleSpec.js
@@ -3,8 +3,6 @@ describe('waterPuzzle', function(){
 
   beforeEach(function(){
     puzzle = new waterPuzzle();
-    jug1 = new fiveLitreJug();
-    jug2 = new threeLitreJug();
   });
 
   it('has an empty five litre jug', function(){
@@ -18,16 +16,38 @@ describe('waterPuzzle', function(){
   });
 
   it('can fill either jug to its maximum capacity', function() {
-    puzzle.fillFromTap(jug1);
-    puzzle.fillFromTap(jug2);
-    expect(jug1.current_capacity).toEqual(5)
-    expect(jug2.current_capacity).toEqual(3)
+    puzzle.fillFiveLitreJug()
+    puzzle.fillThreeLitreJug()
+    expect(puzzle.fiveLitreJug.current_capacity).toEqual(5)
+    expect(puzzle.threeLitreJug.current_capacity).toEqual(3)
   })
 
   it('can empty either jug', function() {
-    puzzle.empty(jug1);
-    puzzle.empty(jug2);
-    expect(jug1.current_capacity).toEqual(0)
-    expect(jug2.current_capacity).toEqual(0)
+    puzzle.emptyFiveLitreJug()
+    puzzle.emptyThreeLitreJug()
+    expect(puzzle.threeLitreJug.current_capacity).toEqual(0)
+    expect(puzzle.fiveLitreJug.current_capacity).toEqual(0)
+  })
+
+  it('pour the contents of one jug into the other', function() {
+    puzzle.fillThreeLitreJug()
+    puzzle.pourInto(puzzle.threeLitreJug, puzzle.fiveLitreJug)
+    expect(puzzle.threeLitreJug.current_capacity).toEqual(0)
+    expect(puzzle.fiveLitreJug.current_capacity).toEqual(3)
+  })
+
+  it('cannot add more water than a jug can hold', function() {
+    puzzle.fillFiveLitreJug()
+    puzzle.fillThreeLitreJug()
+    puzzle.pourInto(puzzle.fiveLitreJug, puzzle.threeLitreJug)
+    expect(puzzle.fiveLitreJug.current_capacity).toEqual(5)
+    expect(puzzle.threeLitreJug.current_capacity).toEqual(3)
+  })
+
+  it('cannot result in negative quanitities of water', function() {
+    puzzle.fillThreeLitreJug()
+    puzzle.pourInto(puzzle.threeLitreJug, puzzle.fiveLitreJug)
+    expect(puzzle.fiveLitreJug.current_capacity).toEqual(3)
+    expect(puzzle.threeLitreJug.current_capacity).toEqual(0)
   })
 })
