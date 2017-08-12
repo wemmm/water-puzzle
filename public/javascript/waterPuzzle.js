1252cdc187d035fde491ed339870b8e62d256da8
wemmm <jenny.wem@gmail.com>
2017-08-12 17:56:11 +0100

    Kills scale object- win/lose states will be extracted to interface

--- public/javascript/waterPuzzle.js
+++ public/javascript/waterPuzzle.js
@@ -1,7 +1,6 @@
 function waterPuzzle() {
   this.fiveLitreJug = new fiveLitreJug;
   this.threeLitreJug = new threeLitreJug;
-  this.scales = new scales;
 }
 
 waterPuzzle.prototype.fillJug = function (jug) {
